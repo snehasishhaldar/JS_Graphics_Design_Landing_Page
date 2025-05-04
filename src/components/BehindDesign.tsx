e094fc60676d11f5f28dd2ff162582b0891941c7
Snehasish Haldar <120680531+snehasishhaldar@users.noreply.github.com>
2025-05-04 20:41:01 +0530

    Update BehindDesign.tsx

--- src/components/BehindDesign.tsx
+++ src/components/BehindDesign.tsx
@@ -71,7 +71,7 @@ const BehindDesign = () => {
         <div className="mt-16 items-center">
           <h3 className="font-rozha text-2xl font-bold text-center mb-8">Bengali Art Elements We Use</h3>
 
-          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-6">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
             <div className="flex flex-col items-center text-center">
               <div className="w-24 h-24 rounded-full  items-center justify-center mb-4">
               <img width="80" height="80" viewBox="0 0 100 100" style="remove-bg"src="/alpana.png" />
